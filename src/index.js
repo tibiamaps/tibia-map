c851f412adaf57bbfe12766a3285d9135533a489
Mathias Bynens <mathias@qiwi.be>
2026-07-19 21:12:19 +0200

    refactor: simplify entry point and overwrite map styling

--- src/index.js
+++ src/index.js
@@ -1,22 +1,5 @@
-// Import Leaflet first so it is available globally for the plugins.
-import L from 'leaflet';
-window.L = L;
-
-// Import plugins (which extend global L).
-import './_js/leaflet.coordinates.js';
-import './_js/leaflet.crosshairs.js';
-import './_js/leaflet.levelbuttons.js';
-import './_js/leaflet.exivabutton.js';
-import './_js/leaflet.markersbutton.js';
-import './_js/leaflet.areasbutton.js';
-import 'leaflet-fullscreen';
-
-// Import map initialization.
+// Import map implementation.
 import './_js/map.js';
 
-// Import stylesheets.
-import 'leaflet/dist/leaflet.css';
-import './_css/leaflet.coordinates.css';
-import './_css/leaflet.buttons.css';
-import 'leaflet-fullscreen/dist/leaflet.fullscreen.css';
+// Import stylesheet.
 import './_css/map.css';
